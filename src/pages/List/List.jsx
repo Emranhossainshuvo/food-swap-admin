af64d47c4f3946645c654c0238a0afcff47060a0
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-27 21:52:08 +0600

    successfully loaded list data from the database and displayed in the list page of admin dashboard

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./List.css";
 import axios from "axios";
-import {toast} from "react-toastify"
+import { toast } from "react-toastify"
 
 const List = () => {
 
@@ -10,22 +10,43 @@ const List = () => {
 
     const fetchList = async () => {
         const response = await axios.get(`${url}/api/food/list`);
+        console.log(response.data)
         if (response.data.success) {
             setList(response.data.data)
         }
-        else{
-            toast.error()
+        else {
+            toast.error("Error")
         }
     }
 
-    useEffect(()=>{
-        
-    })
+    useEffect(() => {
+        fetchList();
+    }, [])
 
 
     return (
-        <div>
-            
+        <div className="list add flex-col">
+            <p>All Food list</p>
+            <div className="list-table">
+                <div className="list-table-format title">
+                    <b>Image</b>
+                    <b>Name</b>
+                    <b>Category</b>
+                    <b>Price</b>
+                    <b>Action</b>
+                </div>
+                {list?.map((item, index) => {
+                    return (
+                        <div key={index} className="list-table-format">
+                            <img src={`${url}/images/`+item.image} alt="" />
+                            <p>{item.name}</p>
+                            <p>{item.category}</p>
+                            <p>${item.price}</p>
+                            <p>X</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 };
