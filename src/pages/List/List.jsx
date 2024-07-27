ff1c59bdd3094feaab6f0b315e624a1a55e851c1
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-27 21:35:54 +0600

    function created to load list data from the database whenever the webpage loaded

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,6 +1,28 @@
-import "./List.css"
+import { useEffect, useState } from "react";
+import "./List.css";
+import axios from "axios";
+import {toast} from "react-toastify"
 
 const List = () => {
+
+    const url = "http://localhost:4000"
+    const [list, setList] = useState([]);
+
+    const fetchList = async () => {
+        const response = await axios.get(`${url}/api/food/list`);
+        if (response.data.success) {
+            setList(response.data.data)
+        }
+        else{
+            toast.error()
+        }
+    }
+
+    useEffect(()=>{
+        
+    })
+
+
     return (
         <div>
             
